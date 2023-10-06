a367f68e7e4fd27d69a2c828407b79c3c2a10c9c
Sam Liu <hello@samliu.dev>
2023-10-06 23:35:17 +0000

    Update build process to match GitHub Pages

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+import os
 from os import path
 from pathlib import Path
 
 from flask import Flask, render_template
 from flask_frozen import Freezer
 
+# Transforms routes to match GitHub Pages format
+# E.g. '/page' -> '/page/index.html' 
+class GH_Freezer(Freezer):
+    def urlpath_to_filepath(self, path):
+        if (not '.' in path.split('/')[-1]) and (not path.split('/')[0].endswith('.html')):
+            path += '/index.html'
+        # Remove the initial slash that should always be there
+        assert path.startswith('/')
+        return path[1:]
 
 template_folder = path.abspath('./wiki')
 
@@ -12,7 +22,11 @@ app = Flask(__name__, template_folder=template_folder)
 app.config['FREEZER_DESTINATION'] = 'public'
 app.config['FREEZER_RELATIVE_URLS'] = True
 app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
-freezer = Freezer(app)
+app.config['FREEZER_DEFAULT_MIMETYPE'] = 'text/html'
+if 'GITHUB_WORKFLOW' in os.environ:
+    freezer = GH_Freezer(app)
+else:
+    freezer = Freezer(app)
 
 @app.cli.command()
 def freeze():
