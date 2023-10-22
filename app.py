63131c2b68eff9247ed876445598b0913116418a
Sam Liu <hello@samliu.dev>
2023-10-21 23:50:28 -0400

    I fixed the wrong thing

--- app.py
+++ app.py
@@ -25,40 +25,10 @@ class GH_Freezer(Freezer):
         assert path.startswith("/")
         return path[1:]
 
-    # def _build_one(self, url, last_modified=None):
-    #     if url[1 : len(self.repo_name) + 1] == self.repo_name:
-    #         url = url[len(self.repo_name) + 1 :]
-    #         print(url)
-    #     return super()._build_one(url, last_modified)
-
-
-# Cheap hack to make static files work on GitHub Pages
-# Prefixes all static file URLs with the repo name
-class GH_Flask(Flask):
-    def __init__(self, *args, **kwargs):
-        self.repo_name = os.environ["GITHUB_REPOSITORY"].split("/")[1]
-        super().__init__(*args, **kwargs)
-
-    def url_for(
-        self,
-        endpoint: str,
-        **values: t.Any,
-    ) -> str:
-        url = super().url_for(endpoint, **values)
-        # if endpoint == "static":
-        #    url = "/" + self.repo_name + url
-        return url
-
 
 template_folder = path.abspath("./wiki")
 static_folder = path.abspath("./static")
-app = None
-if "GITHUB_WORKFLOW" in os.environ:
-    app = GH_Flask(
-        __name__, template_folder=template_folder, static_folder=static_folder
-    )
-else:
-    app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
 
 # app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
 app.config["FREEZER_DESTINATION"] = "build"
